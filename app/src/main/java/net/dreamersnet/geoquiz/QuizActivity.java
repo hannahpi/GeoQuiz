c32d88c3cd02af1c59c08a537e5019a92da9dc62
waterlgndx <bparker@dreamersnet.net>
2016-09-10 23:01:42 -0400

    changes made in Ch 3 of The Big Nerd Ranch Guide to Android Programming

--- app/src/main/java/net/dreamersnet/geoquiz/QuizActivity.java
+++ app/src/main/java/net/dreamersnet/geoquiz/QuizActivity.java
@@ -1,17 +1,23 @@
 package net.dreamersnet.geoquiz;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
     private TextView mQuestionTextView;
+    private static final String TAG = "Quiz Activity";
+    private static final String KEY_INDEX="index";
 
     private Question[] mQuestionsBank = new Question[]{
             new Question(R.string.question_oceans, true),
@@ -24,13 +30,58 @@ public class QuizActivity extends AppCompatActivity {
     private int mCurrentIndex=0;
 
     @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate() called");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        if (savedInstanceState!=null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+        }
         updateQuestion();
 
+        mQuestionTextView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex=(mCurrentIndex +1) % mQuestionsBank.length;
+                updateQuestion();
+            }
+        });
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -45,7 +96,7 @@ public class QuizActivity extends AppCompatActivity {
                 checkAnswer(false);
             }
         });
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -53,6 +104,16 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + mQuestionsBank.length - 1) % mQuestionsBank.length;
+                updateQuestion();
+            }
+        });
+
+
     }
 
     private void updateQuestion() {
